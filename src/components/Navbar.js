6e684c7c567007d89a0bec21a658dbab0fa942b3
ciyuxu <ciyuxu@gmail.com>
2017-10-16 16:42:16 +0200

    SearchBar set-up without style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,14 +19,13 @@ export class Navbar extends PureComponent {
     return (
 
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to={'/'} className='homepage'>StageFinder</Link>
 
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <Link to={'/'} className="nav-item nav-link active" >HomePage <span className="sr-only">(current)</span></Link>
+            <Link to={'/'} className="nav-item nav-link active" >StageFinder <span className="sr-only">(current)</span></Link>
             <Link to={'/stages'} className='stages'>all stages</Link>
             <Link to={'/artists'} className='artists'>all artists</Link>
             <Link to={'/inspirations'} className='inspirations'>inspirations</Link>
