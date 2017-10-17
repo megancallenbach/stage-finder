bb0368b2defff969017e73da1a42ebc0efdce326
megancallenbach <megancallenbach@gmail.com>
2017-10-17 15:00:06 +0200

    Artist page styling and rendering

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,6 @@ export default class HomePage extends PureComponent {
   render() {
     return (
       <div className="home-page">
-        <div className="header-bg">
           <div className="jumbotron">
             <div className="text-container">
               <h2 className="header-text">Iedereen verdient een eigen podium.</h2>
@@ -16,7 +15,6 @@ export default class HomePage extends PureComponent {
               <button className="btn-transparent"><Link to={'/signup'} className="link">Create Profile</Link></button>
             </div>
           </div>
-        </div>
         <div className="two-columns row">
           <div className="image-two col-sm-6">
             <h3 className="white-text"> Heb jij het lef? </h3>
@@ -27,9 +25,6 @@ export default class HomePage extends PureComponent {
             <button className="btn-white"><Link to={'/inspiration'} className="link">Inspiration</Link></button>
           </div>
         </div>
-
-
-
       </div>
     )
   }
--- src/pages/artist/ArtistPage.js
+++ src/pages/artist/ArtistPage.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import fetchArtists from '../../actions/artists/fetch'
+import '../../styles/ArtistPage.css'
+import { Link } from 'react-router'
 
 class ArtistPage extends PureComponent {
 
@@ -9,7 +11,25 @@ class ArtistPage extends PureComponent {
   }
 
   renderArtist(artist){
-    return <li>{artist.name}</li>
+    return (
+      <div className="artist col-sm-4">
+        <h2 className="artist-name">{artist.name}</h2>
+        <div className="artist-box">
+          <div className="artist-image">
+            <img src={artist.photo} alt=""/>
+          </div>
+          <div className="artist-content">
+            <div className="artist-description">
+              <p className="description">{artist.description}</p>
+            </div>
+            <div className="artist-city">
+              <p className="city">{artist.city}</p>
+            </div>
+              <button className="btn-artist"><Link to={`/artists/${artist._id}`} className="link">View Artist</Link></button>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   render() {
@@ -17,10 +37,12 @@ class ArtistPage extends PureComponent {
     if (!artists) return null
     return(
       <div>
-        <h1>Here are all the artists</h1>
-        <ul>
-          { artists.map(this.renderArtist.bind(this)) }
-        </ul>
+        <h1>Discover the artists</h1>
+        <div className="artists-container">
+          <div className="row">
+            { artists.map(this.renderArtist.bind(this)) }
+          </div>
+        </div>
       </div>
     )
   }
