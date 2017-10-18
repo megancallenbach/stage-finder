e65cda5150fd63124cb180138e31e5e0415c1282
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-18 15:16:59 +0200

    Updates to the event detail page

--- src/pages/event/EventDetail.js
+++ src/pages/event/EventDetail.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import fetchEvents from '../../actions/events/fetch'
+import { Link } from 'react-router'
 import '../../styles/EventDetail.css'
 
 class EventDetail extends PureComponent {
@@ -9,9 +10,22 @@ class EventDetail extends PureComponent {
     this.props.fetchEvents()
   }
 
+  joinEvent(){
+
+  }
+
+  renderArtist(artist){
+    return <Link to={`/artists/${artist._id}`}>artist.name</Link>
+  }
+
   render() {
     const event = this.props.events.find((event) => event._id === this.props.params.eventId)
+
     if (!event) return null
+
+    const artists = event.artists
+    const artistCount = (event.artistCount - event.artistIds.length)
+
     return(
       <div className="event-detail">
         <div className="event-photo">
@@ -19,17 +33,21 @@ class EventDetail extends PureComponent {
         </div>
         <div className="row">
           <div className="orange-box col-sm-6">
-            <h1 className="event-name">{event.name}</h1>
+            <h1 className="event-name">{event.title}</h1>
+            <h1 className="event-name">{event.venue.address}, {event.venue.city}</h1>
             <p className="description">{event.description}</p>
           </div>
           <div className="white-box col-sm-6">
-            <h1 className="upcoming">Upcoming</h1>
+            <h1 className="upcoming">Artiesten</h1>
+            <p className="artist-count">We zoeken nog {artistCount} artiesten!</p>
+            { (this.props.currentUser.artistProfileId) ? <button onClick={this.joinEvent.bind(this)}>Join!</button> : null }
+            { (artists) ? artists.map(this.renderArtist.bind(this)) : null }
           </div>
         </div>
       </div>
     )
   }
 }
-const mapStateToProps = ({ events }) => ({ events })
+const mapStateToProps = ({ events, currentUser }) => ({ events, currentUser })
 
 export default connect(mapStateToProps, { fetchEvents })(EventDetail)
