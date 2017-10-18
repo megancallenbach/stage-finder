2f0f412c433f000e30e0a9437d765b65fe29afc3
megancallenbach <megancallenbach@gmail.com>
2017-10-18 14:14:56 +0200

    Changed backgrounds, some additional styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <div className="scroll">  
-          {this.props.children}
-          <Footer />
-        </div>
+        {this.props.children}
       </div>
     );
   }
