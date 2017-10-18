7c642809a99e1fd405067de4340a2ce9ece03b1b
jobweeda <jobweeda@hotmail.com>
2017-10-18 16:31:33 +0200

    takes the currentVenue out of the database when going to the detailpage of that particular venue

--- src/pages/venue/VenueDetail.js
+++ src/pages/venue/VenueDetail.js
@@ -1,28 +1,28 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import fetchVenues from '../../actions/venues/fetch'
 import '../../styles/VenueDetail.css'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
+import getVenue from '../../actions/venues/get'
 
 class VenueDetail extends PureComponent {
   componentWillMount(){
-    this.props.fetchVenues()
+    debugger
+    this.props.getVenue(this.props.params.venueId)
   }
 
   render() {
-    const venue = this.props.venues.find((venue) => venue._id === this.props.params.venueId)
-    if (!venue) return null
+    if (!this.props.currentVenue) return null
     return(
       <div className="venue-detail">
         <Navbar/>
         <div className="venue-photo">
-          <img className="image-responsive" src={venue.photo} alt=""/>
+          <img className="image-responsive" src={this.props.currentVenue.photo} alt=""/>
         </div>
         <div className="row">
           <div className="orange-box col-sm-6">
-            <h1 className="venue-name">{venue.name}</h1>
-            <p className="description">{venue.description}</p>
+            <h1 className="venue-name">{this.props.currentVenue.name}</h1>
+            <p className="description">{this.props.currentVenue.description}</p>
           </div>
           <div className="white-box col-sm-6">
             <h1 className="upcoming">Upcoming</h1>
@@ -33,6 +33,6 @@ class VenueDetail extends PureComponent {
     )
   }
 }
-const mapStateToProps = ({ venues }) => ({ venues })
+const mapStateToProps = ({ currentVenue }) => ({ currentVenue })
 
-export default connect(mapStateToProps, { fetchVenues })(VenueDetail)
+export default connect(mapStateToProps, { getVenue })(VenueDetail)
