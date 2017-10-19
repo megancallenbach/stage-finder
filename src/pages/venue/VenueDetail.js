f283a659d511fa3a11d698c4769d20ba6dcf030d
jobweeda <jobweeda@hotmail.com>
2017-10-19 14:29:58 +0200

    might add a possible way to sort the dates but it isnt owrking yet

--- src/pages/venue/VenueDetail.js
+++ src/pages/venue/VenueDetail.js
@@ -6,19 +6,30 @@ import Footer from '../../components/Footer'
 import getVenue from '../../actions/venues/get'
 
 
+
 class VenueDetail extends PureComponent {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = { dates }
+  //   this.sortByDates = this.sortByDates.bind(this)
+  // }
+  // sortByDates() {
+  //   this.setState(prevState => {
+  //     this.state.dates.sort((a, b) => (a.dates - b.dates))
+  //   })
+  // }
+
   componentWillMount(){
     this.props.getVenue(this.props.params.venueId)
   }
 
-
-
-
-
   render() {
-    // const sortedEvents = this.props.currentVenue.events.sort(Date)
 
+    // const sortedEvents = this.props.currentEvent.sort(Date)
+    //
+    // console.log(sortedEvents)
     if (!this.props.currentVenue) return null
+    debugger
     return(
       <div className="venue-detail">
         <Navbar/>
@@ -29,15 +40,13 @@ class VenueDetail extends PureComponent {
           <div className="orange-box col-sm-6">
             <h1 className="venue-name">{this.props.currentVenue.name}</h1>
             <p className="description">{this.props.currentVenue.address}</p>
-
           </div>
           <div className="white-box col-sm-6">
             <h1 className="upcoming">Upcoming</h1>
             <p className="venue-events">{this.props.currentVenue.events[0].title}</p>
-
-            <p className="venue-date">{ this.props.currentVenue.events[0].date }</p>
+            <p className="venue-date">{this.props.currentVenue.events[0].date}</p>
+              </div>
           </div>
-        </div>
         <Footer />
       </div>
     )
