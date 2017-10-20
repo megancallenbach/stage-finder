e4bbed782aa1b3c34c7b61d6347114f679bd68d2
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-20 16:59:33 +0200

    Makes it so that the client communicates directly to the elasticsearch api

--- src/actions/search/search.js
+++ src/actions/search/search.js
@@ -1,41 +1,45 @@
-import API from '../../api'
+import elasticsearch from 'elasticsearch'
 import { history } from '../../store'
 
-export const SEARCH_INPUT = 'SEARCH_INPUT'
 export const SEARCH_RESULTS = 'SEARCH_RESULTS'
+export const SEARCH_INPUT = 'SEARCH_INPUT'
 
-const api = new API()
+const client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
 
 export default (searchInput) => {
   return (dispatch) => {
 
-    const backend = api.service('search')
-
-    backend.find({
-      query: {
-        $sqs: {
-          $fields: [
-            '_all'
-          ],
-          $query: searchInput,
-          $operator: 'and'
+    client.search({
+      index: 'events',
+      type: 'event',
+      body: {
+        query: {
+          simple_query_string: {
+            query: searchInput,
+            fields: ['_all'],
+            default_operator: 'and'
+          }
         }
       }
     })
-      .then((result) => {
-        console.log(result)
-        dispatch({
-          type: SEARCH_INPUT,
-          payload: searchInput
+      .then((results) => {
+        console.log(results.hits.hits)
+
+        const convertedResults = results.hits.hits.map((result) => {
+          const id = result._id
+          const event = result._source
+          return { ...event, _id: id }
         })
-        dispatch({
-          type: SEARCH_RESULTS,
-          payload: result
-        })
+
+        dispatch({type: SEARCH_RESULTS, payload: convertedResults})
+        dispatch({type: SEARCH_INPUT, payload: searchInput})
         history.push('/search-results')
       })
       .catch((error) => {
-        console.error(error)
+        console.log(error)
       })
   }
 }
