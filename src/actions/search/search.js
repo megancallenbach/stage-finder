5160368b901aad3a890bcf74c0a447b0ec83d741
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-20 11:37:13 +0200

    Adds a search action

---
+++ src/actions/search/search.js
@@ -0,0 +1,31 @@
+import API from '../../api'
+
+export const SEARCH = 'SEARCH'
+
+const api = new API()
+
+export default (searchInput) => {
+  return (dispatch) => {
+
+    const backend = api.service('search')
+
+    backend.get({
+      query: {
+        $sqs: {
+          $fields: [
+            '_all'
+          ],
+          $query: searchInput,
+          $operator: 'and'
+        }
+      }
+    })
+      .then((result) => {
+        console.log(result)
+
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+}
