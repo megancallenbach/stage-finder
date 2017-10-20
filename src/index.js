aac44c6ce5402fc276a2ad15407102a9e99183f9
megancallenbach <megancallenbach@gmail.com>
2017-10-20 11:52:27 +0200

    Carousel working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
