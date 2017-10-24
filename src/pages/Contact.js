19d2cebc6029e4814e4206a6eea22f24f423a86f
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-24 09:43:57 +0200

    Adds Jobs new pretty API key for the google maps component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -59,7 +59,7 @@ class Contact extends PureComponent {
             </div>
             <div className="map col-sm-6">
               <MapWithAMakredInfoWindow
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbdQeu9m0v1KRq_hU5S-UYvoQJqcOBAuY&v=3.exp&libraries=geometry,drawing,places"
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ margin: '15px', height: `220px`, width: `370px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
